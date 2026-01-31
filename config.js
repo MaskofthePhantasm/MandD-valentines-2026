b0854f4e43de0aaeeabc02de7cdfe9203b593c9e
MaskofthePhantasm <daamansingh79@gmail.com>
2026-01-31 11:22:14 -0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/depg2ougn/video/upload/v1769887266/She___Him_-_I_Thought_I_Saw_Your_Face_Today__Official_Lyric_Video__128k_v3zec2.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
